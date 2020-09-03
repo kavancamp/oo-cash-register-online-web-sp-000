21aec926578906e27543cbc4ac878fc9d880c53c
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-03 01:53:14 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,21 +8,19 @@ class CashRegister
     @discount = discount
     @items = []
   end
-
-
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
+  
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
+    if quantity > 1
+      counter = 0
+      while counter < quantity
+        @items << item
+        counter += 1
       end
     else
-      @items << title
+      @items << item
     end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
   end
 
   def apply_discount()
@@ -37,7 +35,7 @@ class CashRegister
 
 
   def void_last_transaction()
-    @total -= @last_transaction_amount
+    @total -= @price
   end
 
 end
