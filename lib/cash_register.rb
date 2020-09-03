3a72b4d9089fdb5d6d677b5ba24ecdf381c7c0a0
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-03 01:46:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,12 +7,12 @@ class CashRegister
    @items = []
  end
 
- def add_item(title, price, quantity = 1)
-    if quantity > 1
-      i = 0
-      while i < quanity
+ def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
         @items << title
-        i += 1
+        i+=1
       end
     else
       @items << title
