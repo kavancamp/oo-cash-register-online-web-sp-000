f91bac7c412f7c0d32d8b8f5d57a2e975318016b
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-03 01:44:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -22,8 +22,15 @@ class CashRegister
     @total
   end
 
-
-
+  def apply_discount()
+    if @discount > 0 
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
 
 
 end
