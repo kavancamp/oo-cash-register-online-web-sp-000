04ef3c907b2e218a82a12f956496b34afb006c63
Keenah Vancampenhout <kvancamp@mail.com>
2020-09-03 01:41:54 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+ attr_accesor :total, :discount, :items, :last_transaction_amount
 
+ def initialize(discount = 0)
+   @total = 0
+   @discount = discount
+   @items = []
+ end
+
+ def add_item(title, price, quantity = 1)
+    if quantity > 1
+      i = 0
+      while i < quanity
+        @items << title
+        i += 1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
+
+
+
+
+
+end
